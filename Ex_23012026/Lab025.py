bd8b2664cd1ab9b52ddb66a2d7c52ff52e0733d6
amritanshu98 <amrit0498@gmail.com>
2026-01-24 20:01:46 +0530

    Python Basics

---
+++ Ex_23012026/Lab025.py
@@ -0,0 +1,13 @@
+# Literals
+# var_name = variable_value
+name = "Amrit"
+# var_name -> Identifier
+# var_val -> Literals
+#  are the actual values assigned
+# Literals can be Numeric and Non-Numeric.
+age = 12
+pi = 3.14
+is_married = True
+have_lambo = None
+my_list = []
+my_set = {}
---
+++ Ex_23012026/Lab034.py
@@ -0,0 +1,3 @@
+#One Line Code for Area of Circle
+
+print(3.141592653589793*(float(input("Enter the radius\n"))**2))
