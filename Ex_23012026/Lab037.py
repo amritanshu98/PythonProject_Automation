229fdf82b0f91ba5d3f782e048961758bafc9e49
amritanshu98 <amrit0498@gmail.com>
2026-01-24 20:43:22 +0530

    new

--- Ex_23012026/Lab037.py
+++ Ex_23012026/Lab037.py
@@ -1,5 +1,5 @@
 num1 = float(input("Enter the First Number\n"))
 num2 = float(input("Enter the Second Number\n"))
 
-print(f'First Number is greater: {num1} than Second Number {num2}' if num1>num2 else f'Second Number is greater: {num2} than First Number {num1}')
+print(f'First Number is greater({num1}) than Second Number ({num2})' if num1>num2 else f'Second Number is greater ({num2}) than First Number ({num1})')
 
