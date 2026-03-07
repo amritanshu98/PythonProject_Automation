749bad6ea109fcd579fe67d0d15f99372c1dd26b
amritanshu98 <amrit0498@gmail.com>
2026-03-07 23:31:14 +0530

    Python Basics

---
+++ Ex_26032026/Lab088.py
@@ -0,0 +1,9 @@
+q, w, e = (45, 56, 78)
+print(q)
+print(w)
+print(e)
+
+# Search in Tuple
+cities = ("London", "Paris", "Los Angeles", "Tokyo")
+print("Paris" in cities)
+print("New Delhi" in cities)
